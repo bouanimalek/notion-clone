139f66f0bfc10f03367634dd2ae98580ff498c26
malek <malzovich@gmail.com>
2023-10-26 16:39:07 +0100

    feat: Document List

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -15,15 +15,15 @@ import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
 import { Item } from "./item";
 
-import { useMutation, useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
+import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
 
-  const documents = useQuery(api.documents.get);
   const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
@@ -146,9 +146,7 @@ export const Navigation = () => {
           <Item onClick={handleCreate} label={"New Page"} icon={PlusCircle} />
         </div>
         <div className="mt-4">
-          {documents?.map((document) => (
-            <p key={document._id}>{document.title}</p>
-          ))}
+          <DocumentList />
         </div>
         <div
           onMouseDown={handleMouseDown}
