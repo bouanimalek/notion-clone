aa9acaea87128a3a743345a6a020aa75a023e2cc
malek <malzovich@gmail.com>
2023-10-27 16:37:44 +0100

    feat: Advanced file upload (delete, replace)

--- components/cover.tsx
+++ components/cover.tsx
@@ -10,6 +10,7 @@ import { api } from "@/convex/_generated/api";
 import { useCoverImage } from "@/hooks/use-cover-image";
 import { Button } from "@/components/ui/button";
 import { Id } from "@/convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
 
 interface CoverImageProps {
   url?: string;
@@ -20,8 +21,14 @@ export const Cover = ({ url, preview }: CoverImageProps) => {
   const params = useParams();
   const coverImage = useCoverImage();
   const removeCoverImage = useMutation(api.documents.removeCoverImage);
+  const { edgestore } = useEdgeStore();
 
-  const onRemove = () => {
+  const onRemove = async () => {
+    if (url) {
+      await edgestore.publicFiles.delete({
+        url: url,
+      });
+    }
     removeCoverImage({ id: params.documentId as Id<"documents"> });
   };
   return (
@@ -36,7 +43,7 @@ export const Cover = ({ url, preview }: CoverImageProps) => {
       {url && !preview && (
         <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
           <Button
-            onClick={coverImage.onOpen}
+            onClick={() => coverImage.onReplace(url)}
             className="text-muted-foreground text-xs"
             variant={"outline"}
             size={"sm"}
